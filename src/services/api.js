e6cf11635491f3ccd2768ccb583bbd3d8b93b888
rodryv99 <dryv99@gmail.com>
2025-06-05 04:09:13 -0400

    act1

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://smart-class-backend-d10err24d50c73anjuhg.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
